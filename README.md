58e88e597bf8da861402bdc10edc13737b5305b9
Noam Siegel <52804845+noamsiegel@users.noreply.github.com>
2023-05-18 01:08:46 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Abstract
+
+This Python script utilizes OpenAI's language model to summarize the content of emails fetched from an IMAP email server.
+
+The script connects to the email server, retrieves the desired emails, processes their content, and uses the language model to generate concise summaries.
+
+The summaries can be useful for quickly understanding the key points of the emails without reading them in detail.
+
+The script also handles HTML content, removes URLs, and supports splitting long emails into smaller chunks for more accurate summarization.
+
+## Things to add
+
+- Aggregate email summarizes and send a summary at 24:00 every day
+  - Either through email
+  - Or through text, email by email
+- Archive emails that have been summarized
+  - Or moved them to another folder?
+- Make setting up process simpler
+
+## Getting started
+
+To run the provided script for email summarization using OpenAI's language model, follow these steps:
+
+Install the required dependencies: Ensure you have Python installed on your system. Additionally, install the necessary packages by running the command pip install langchain openai-python-client html2text python-dotenv.
+
+1. Set up the environment variables: Create a file named .env in the same directory as the script. Open the .env file and provide the required environment variables:
+
+- `OPEN_AI_PAT`: Your OpenAI API key.
+- `IMAP_SERVER`: The IMAP server address for your email provider.
+- `EMAIL_ADDRESS`: Your email address.
+- `EMAIL_PASSWORD`: Your email password.
+
+2. Customize the script (optional): Review the script and make any necessary modifications. For example, you can adjust the summarization settings or modify the prompt used for summarization.
+
+3. Run the script: Execute the script by running the command python script_name.py, replacing script_name.py with the actual name of the script file.
+
+Ensure that you have a stable internet connection and access to the configured IMAP email server. The script will work for the most recent emails in your inbox, but you can adjust the range of messages to process by modifying the num_of_messages variable.
+
+Note: Make sure to handle your API key and other sensitive information securely and avoid sharing it publicly.
---
+++ email_summarizer_v1.py
@@ -0,0 +1,124 @@
+import os
+import imaplib
+import email
+import re
+import html2text
+import urllib.parse
+import logging
+
+from langchain import OpenAI
+from langchain.chains.summarize import load_summarize_chain
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+
+from dotenv import load_dotenv
+load_dotenv()
+
+OPEN_AI_PAT = os.environ['OPEN_AI_PAT']
+IMAP_SERVER = os.getenv('IMAP_SERVER')
+EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+
+def remove_urls(text):
+    # Remove URLs using regex pattern
+    return re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text)
+
+def get_text(msg):
+    if msg.is_multipart():
+        return get_text(msg.get_payload(0))
+    else:
+        payload = msg.get_payload()
+        content_type = msg.get_content_type()
+        if content_type == "text/html":
+            payload = html2text.html2text(payload)
+            payload_text = re.sub(r'\[([^]]+)\]\([^)]+\)', r'\1', payload)
+            return remove_urls(payload_text)
+        else:
+            payload_text = re.sub(r'\[([^]]+)\]\([^)]+\)', r'\1', payload)
+            return remove_urls(payload_text)
+
+mail = imaplib.IMAP4_SSL(IMAP_SERVER)
+mail.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+mail.list()
+mail.select("inbox")
+
+status, messages = mail.select("INBOX")
+num_of_messages = int(messages[0])
+
+llm = OpenAI(
+    temperature=0,
+    openai_api_key=OPEN_AI_PAT,
+    model_name="text-davinci-003"
+)
+
+# Load the summarization chain
+chain = load_summarize_chain(llm, chain_type="refine")
+
+for i in range(num_of_messages, num_of_messages - 3, -1):
+	result, data = mail.fetch(str(i), "(RFC822)")
+	raw_email = data[0][1]
+
+	email_message = email.message_from_bytes(raw_email)
+	# keys = email_message.keys()
+	# for key in keys:
+	# 	print(key)
+	gmail_id = email_message['X-Gmail-Message-Id']
+	sender = email_message['From']
+	subject = email_message['Subject']
+
+	body = get_text(email_message)
+
+	try:
+		body = body.decode()
+	except:
+		pass
+
+	urls = list(set(urllib.parse.urlsplit(url) for url in re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', body)))
+
+	body_clean = re.sub('<[^>]*>', '', body)
+	body_clean = re.sub('[^A-Za-z0-9.?!,:; \n]+', '', body_clean)
+
+	text_splitter = RecursiveCharacterTextSplitter(
+		chunk_size=1000, 
+		chunk_overlap=50,
+		separators=[" ", ",", "\n"]
+	)
+    
+	chunks = text_splitter.split_text(body)
+
+	documents = []
+
+	# Iterate over the chunks and create Document objects with page_content
+	if len(chunks) <= 10:
+		logging.info("Chunks picked")
+		for chunk in chunks:
+			logging.info("Creating Document object")
+			document = Document(page_content=chunk)
+			documents.append(document)
+
+	if documents:
+		prompt_document = Document(page_content="Summarize the following email into bullet points ordered by importance:\n")
+
+		documents.insert(0, prompt_document)
+		summary = chain(documents)
+		summarized_text = summary['output_text']
+
+		print("CHUNKS:",len(chunks))
+		print("SENDER:", sender)
+		print("SUBJECT:", subject)
+		print("SUMMARY:\n", summarized_text)
+		# print(email_url)
+		print("----------\n")
+	else:
+		logging.warning("Too many chunks")
+		print("CHUNKS:",len(chunks))
+		print("SENDER:", sender)
+		print("SUBJECT:", subject)
+		# print(email_url)
+		print("----------\n")
+		# print("CLEAN BODY: \n", body_clean)
+		# print("URLs:\n", urls)
+		
+	
+
